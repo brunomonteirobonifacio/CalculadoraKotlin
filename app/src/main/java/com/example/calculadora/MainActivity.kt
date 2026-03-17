83e8f9eb3ae2bbe2305da8e15fb05070d5b4978e
bruno.343923 <bruno.343923@alunosatc.edu.br>
2026-03-16 21:17:48 -0300

    Initial commit

---
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.example.calculadora
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Home()
+        }
+    }
+}
+
+@Composable
+fun Home() {
+    val expression = MathExpression(NumberToken(0))
+
+    val observableExpressionValue by remember { mutableStateOf(expression.tokens) }
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(30.dp),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ){
+            Text(fontSize = 50.sp, text = observableExpressionValue.toString())
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            CalculatorButton(7, expression)
+            CalculatorButton(8, expression)
+            CalculatorButton(9, expression)
+            CalculatorButton(OperatorToken.DIVIDE, expression)
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            CalculatorButton(4, expression)
+            CalculatorButton(5, expression)
+            CalculatorButton(6, expression)
+            CalculatorButton(OperatorToken.MULTIPLY, expression)
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            CalculatorButton(1, expression)
+            CalculatorButton(2, expression)
+            CalculatorButton(3, expression)
+            CalculatorButton(OperatorToken.SUBTRACT, expression)
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            CalculatorButton(0, expression)
+            CalculatorButton("C", { expression.clear() }, expression)
+            CalculatorButton("=", { expression.calculate() }, expression)
+            CalculatorButton(OperatorToken.SUM, expression)
+        }
+    }
+}
+
+@Composable
+fun CalculatorButton(value: Int, expression: MathExpression) {
+    CalculatorButton(NumberToken(value), expression)
+}
+
+@Composable
+fun CalculatorButton(token: Token, expression: MathExpression) {
+    CalculatorButton(token.symbol, { it.append(token) }, expression)
+}
+
+@Composable
+fun CalculatorButton(text: String, action: (MathExpression) -> Unit, expression: MathExpression) {
+    Button(
+        modifier = Modifier
+            .padding(5.dp),
+        onClick = { action(expression) }
+    ) {
+        Text(fontSize = 30.sp, fontWeight = FontWeight.Light, text = text)
+    }
+}
+
+@Preview
+@Composable
+fun HomePreview() {
+    Home()
+}
---
+++ app/src/main/java/com/example/calculadora/MathExpression.kt
@@ -0,0 +1,65 @@
+package com.example.calculadora
+
+class MathExpression {
+    val value: String
+        get() = tokens
+            .map { it.symbol }
+            .joinToString(separator = " ") { it }
+
+    constructor(token: Token) {
+        tokens.add(token)
+    }
+
+    val tokens: MutableList<Token> = mutableListOf()
+
+    fun append(token: Token) {
+        if (tokens.size < 3) tokens.add(token)
+    }
+
+    fun clear() = tokens.clear()
+
+    fun calculate() {
+        if (tokens.isEmpty()) return
+
+        var accumulator = (tokens[0] as NumberToken).number
+        var operator: OperatorToken? = null
+        var current: Int? = null
+
+        val iterator = tokens.iterator()
+        iterator.next()
+
+        while (iterator.hasNext()) {
+            val token = iterator.next()
+
+            if (operator != null) {
+                if (token is OperatorToken) throw IllegalStateException("Invalid syntax")
+                current = (token as NumberToken).number
+
+                accumulator = operator.exec(accumulator, current)
+            } else {
+                if (token is NumberToken) throw IllegalStateException("Invalid syntax")
+                operator = (token as OperatorToken)
+                current = null
+            }
+        }
+
+        tokens.clear()
+        tokens.add(NumberToken(accumulator))
+    }
+}
+
+data class NumberToken(val number: Int) : Token {
+    override val symbol: String = number.toString()
+}
+
+enum class OperatorToken(override val symbol: String, val exec: (Int, Int) -> Int) : Token {
+    SUM("+", { a, b -> a + b }),
+    SUBTRACT("-", { a, b -> a - b }),
+    DIVIDE("/", { a, b -> a / b }),
+    MULTIPLY("*", { a, b -> a / b }),
+    ;
+}
+
+interface Token {
+    val symbol: String
+}
